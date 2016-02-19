47906ba897a8db11696f6b3f737283fc8a3f557a
CorainChicago <chaysmagan@gmail.com>
2016-02-19 09:40:30 -0600

    create method get_total, test passing

--- order.rb
+++ order.rb
@@ -1,15 +1,23 @@
 class Order
+  attr_accessor :menu_array, :total
+
+  def initialize
+    @menu_array = []
+    total = nil
+  end
 
   def instructions
     p "Please enter the file you want to upload"
   end
 
   def parse(filename)
-    menu_array = []
     File.readlines(filename).each do |line|
-     menu_array << line.chop
+     @menu_array << line.chop
     end
-    menu_array
+  end
+
+  def get_total
+    total = @menu_array[0]
   end
 
 end
