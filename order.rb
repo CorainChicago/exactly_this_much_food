eaf30b4d99c7ebd5da7db231f60af6d63dc466db
CorainChicago <chaysmagan@gmail.com>
2016-02-20 00:39:31 -0600

    fix error in add_prices from the issues with Ruby and floats, create add_items method to recurse for solutions

--- order.rb
+++ order.rb
@@ -39,15 +39,39 @@ class Order
   end
 
   def add_prices(items)
-    sum = 0
+    sum = 0.00
     items.each do |item|
-     sum += @menu_hash[item].delete("$").to_f
+      sum += @menu_hash[item].delete("$").to_f
     end
-    return sum
+    return '%.02f' % sum
   end
 
-  def possible_orders
+  def find_orders
+    @solutions = []
+    @menu_keys_array = Array.new(@menu_hash.keys)
+    possible_order = []
+    add_items(possible_order, 0)
+    return @solutions
   end
+
+
+  def add_items(possible_order, count)
+    for i in 0..@menu_keys_array.length - 1
+      temp_order = possible_order.map{|item| item}
+      temp_order << @menu_keys_array[i]
+      temp_order.sort!
+      p temp_order
+      if add_prices(temp_order) == @total
+        unless @solutions.include?(temp_order)
+          @solutions << temp_order
+          return @solutions
+        end
+      elsif add_prices(temp_order).to_f < @total.to_f
+        add_items(temp_order, count)  
+      end
+    end
+  end
+
 end
 
 
