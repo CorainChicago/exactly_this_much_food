cfed1bafbf736da9f2e234858f6d4be4cf5743bf
CorainChicago <chaysmagan@gmail.com>
2016-02-26 11:49:12 -0600

    remove inheritance for OrderPresenter, move runner methods to OrderPresenter

--- order_presenter.rb
+++ order_presenter.rb
@@ -1,4 +1,23 @@
 require_relative 'order.rb'
 
-class OrderPresenter << Order
+class OrderPresenter
+  attr_accessor :order
+
+  def initialize
+    @order = Order.new
+  end
+
+  def run
+    order.welcome
+    order.load_file
+    order.check_filename
+    order.parse
+    order.get_target_price
+    order.menu_array
+    order.convert_array_to_hash
+    order.find_orders
+    order.format_results
+    order.message_results
+    order.offer_to_repeat
+  end
 end
