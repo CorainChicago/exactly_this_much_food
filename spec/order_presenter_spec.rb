559070bc01cd1ea5e3400bda7f35d7251730bb7b
CorainChicago <chaysmagan@gmail.com>
2016-02-28 01:20:05 -0600

    add test for message_results to cover no solutions

--- spec/order_presenter_spec.rb
+++ spec/order_presenter_spec.rb
@@ -28,6 +28,13 @@ RSpec.describe OrderPresenter do
       output = capture_standard_output { presenter.message_results("15.05", solutions, formatted_results)}
       expect(output).to eq "\nYou have 2 options which add up to the target price of $15.05.\n\n\nHere is option 1: \n\n7 of the mixed fruit\n\n\nHere is option 2: \n\n2 of the hot wings\n1 of the mixed fruit\n1 of the sampler plate"
     end
+
+    it "prints an error message if the solutions array is empty" do
+      solutions = []
+      formatted_results = []
+      output = capture_standard_output { presenter.message_results("15.05", solutions, formatted_results)}
+      expect(output).to eq "\nWe didn't find any possible combinations to match the amount you want to spend. You can update the file and rerun the program."
+    end
   end
 
 #   it "displays an error message if no solutions are found" do
